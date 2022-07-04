203898ab9ef513d55a9b05146b9fe059bd737291
raviteja123123 <kavetijeevansai@gmail.com>
2022-07-04 14:25:59 +0530

    function changed

--- add.c
+++ add.c
@@ -4,9 +4,17 @@ void add(int a,int b)
 	printf("%d",y);
 }
 
+<<<<<<< HEAD
 void sub(int a1,int b1)
 {
 	int y1=a1-b1;
 	printf("%d",y1);
 }
 
+=======
+void sub(int a,int b)
+{
+	int y=a-b;
+	printf("%d",y);
+}
+>>>>>>> db89472a7a7b388f5475507eb5daa427d61a0afa
