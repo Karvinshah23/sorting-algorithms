6bd4c99766041e510054bca1dc16d6278904eacc
raviteja123123 <kavetijeevansai@gmail.com>
2022-07-04 16:17:08 +0530

    changed

--- add.c
+++ add.c
@@ -11,3 +11,10 @@ void sub(int a1,int b1)
         int y=a-b;
         printf("%d",y);
 }
+void div(int a1,int b1)
+{
+	int y1=a1/b1;
+	printf("%d",y1);
+        int y=a/b;
+        printf("%d",y);
+}
