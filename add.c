7d756202369bb8568f24e688700131f35ae4dd24
Akshara Gudur <gudurakshara30@gmail.com>
2022-07-04 11:10:32 +0530

    add example

--- add.c
+++ add.c
@@ -1,16 +1,17 @@
 void add(int a,int b)
 {
-	int y=a+b;
-	printf("%d",y);
+        int y=a+b;
+        printf("%d",y);
 }
 
 void sub(int a,int b)
 {
-	int y=a-b;
-	printf("%d",y);
+        int y=a-b;
+        printf("%d",y);
 }
 void mul(int a,int b)
 {
-	int c=a*b;
-	printf("%d",c)
+        int c=a*b;
+        printf("%d",c)
 }
+
